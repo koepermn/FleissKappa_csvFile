fadf2b1919ea421de3dcde4c920a3a1a2f19d5f8
koepermn <maximilian.koeper@gmail.com>
2018-01-09 09:22:44 +0100

    Add files via upload
    added function for cophens kappa

--- code/fleiss_kappa.R
+++ code/fleiss_kappa.R
@@ -1,6 +1,6 @@
 library("irr")
 
-#library("dplyr")
-#data <- read.csv("full-an/an_task-4.csv", sep =",", header=TRUE) 
-data <- read.csv.sql("example_input.csv", sep =",", header=TRUE)               
-print(kappam.fleiss(data,exact=FALSE, detail=FALSE))
+
+data <- read.csv("example_input.csv", sep =",", header=TRUE)               
+print(kappam.fleiss(data,exact=FALSE, detail=FALSE)) #<- for More than 2 Annot = FLeiss kappa
+#print(kappa2(data,"unweighted")) # Cohens Kappa for 2 Annot
